34567ceaed8e8b8e998fcaecca07cd97930abe1d
bagg1487 <kutenand2@gmail.com>
2024-10-30 01:18:39 +0700

    Update laba_6.2.c

--- laba_6/laba_6.2.c
+++ laba_6/laba_6.2.c
@@ -3,16 +3,13 @@ int main() {
     int A[5]={2,33,4,56,23};
     int B[5]={-34,25,-98,23,-3};
     int C[5]={0};
-    int max_A = A[0], negative_B=0,sum_B=0;
+    int max_A = A[0], negative_B=0,sum_C=0;
     for(int i;i<5;i++){
         if (B[i]<0){
            negative_B++;}
         C[i]=A[i]+B[i];
-        sum_B += C[i];
+        sum_C += C[i];
         if (max_A<A[i]){
             max_A=A[i];}
-    }
-    
-    
-    printf("Max in A: %d \n Count negative in B: %d \n Average summary C: %d \n",max_A,negative_B,sum_B);
+    } printf("Max in A: %d \n Count negative in B: %d \n Average summary C: %.3lf \n",max_A,negative_B,(double)sum_C/5);
 }
