695021eadebd5b01436e9b99f0007169319094a3
bagg1487 <kutenand2@gmail.com>
2024-10-31 01:41:53 +0700

    Update Laba_7.c

--- laba_7/Laba_7.c
+++ laba_7/Laba_7.c
@@ -1,11 +1,12 @@
-//Записать слова в предложении в обратном порядке.
+//Определить, есть ли в заданном предложении цифры. И если есть, найти их сумму.
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 int main(){
-    char input_str[256],a;
+     char input_str[256];
+     int sum=0;
     fgets(input_str,256,stdin);
-    for(int i = strlen(input_str);i>=0;i--){
-        char a = input_str[i];
-        printf("%c",a); 
+    for(int i = 0;i<strlen(input_str);i++){
+        if (isdigit(input_str[i])) sum+=input_str[i]-'0';
+        } printf("%d\n",sum);  
     }
-}
