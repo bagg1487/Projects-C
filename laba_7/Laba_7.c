3c1284a183c2b273eb4f69f6171858993e5ed04c
bagg1487 <kutenand2@gmail.com>
2024-10-31 00:20:01 +0700

    Update Laba_7.c

--- laba_7/Laba_7.c
+++ laba_7/Laba_7.c
@@ -1,16 +1,11 @@
 //Записать слова в предложении в обратном порядке.
 #include <stdio.h>
 #include <string.h>
-int main() {
-    char input_str[256],*words[256];
-    int word_count = 0;
-    fgets(input_str, sizeof(input_str), stdin);
-    char *word = strtok(input_str, " \n");
-    for (word_count;word!=0;word_count++){
-        words[word_count] = word;
-        word = strtok(0, " \n"); 
+int main(){
+    char input_str[256],a;
+    fgets(input_str,256,stdin);
+    for(int i = strlen(input_str);i>=0;i--){
+        char a = input_str[i];
+        printf("%c",a); 
     }
-    for (int i = word_count - 1; i >= 0; i--) {
-        printf("%s ", words[i]);
-    } printf("\n");
 }
