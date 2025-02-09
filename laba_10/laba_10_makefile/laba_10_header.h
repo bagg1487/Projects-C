f228c83200cbc30fbce86c8cf2c5596b76d78804
bagg1487 <kutenand2@gmail.com>
2025-02-09 15:22:14 +0700

    Delete laba_10 directory

--- laba_10/laba_10_makefile/laba_10_header.h
+++
@@ -1,6 +0,0 @@
-#define FUNCTIONS_H
-#include <stdio.h>
-struct fraction {float a,b;};
-void multipication(float a,float b);
-void division (float a,float b);
-
--- laba_10/laba_10_makefile/laba_10_other.c
+++
@@ -1,3 +0,0 @@
-#include "laba_10_header.h"
-void multipication(float a,float b){printf("%.4f\n", a*b);}
-void division (float a,float b){printf("%.4f\n", a/b);}
