fb7483a7bedd1a0365eb8c890db755c2b3a25afc
bagg1487 <kutenand2@gmail.com>
2025-02-17 22:22:27 +0700

    Update prog_1_lab.c

--- 2 semester/programming/prog_1_lab.c
+++ 2 semester/programming/prog_1_lab.c
@@ -5,31 +5,31 @@
 #define N 100
 #define MAX 300
 
-struct Student{
+struct Student {
     char name[64];
-    int math,phy,inf,sum;
+    int math, phy, inf, sum;
 };
 
-struct Student AddStudent(char *name, int math, int phy, int inf){
+struct Student AddStudent(char *name, int math, int phy, int inf) {
     struct Student NewStudent;
-    strncpy(NewStudent.name,name,sizeof(NewStudent.name)-1);
-    NewStudent.name[sizeof(NewStudent.name)-1] = '\0';
+    strncpy(NewStudent.name, name, sizeof(NewStudent.name) - 1);
+    NewStudent.name[sizeof(NewStudent.name) - 1] = '\0';
     NewStudent.math = math;
     NewStudent.phy = phy;
     NewStudent.inf = inf;
     NewStudent.sum = math + inf + phy;
     return NewStudent;
-    }
+}
 
-void printStudentInfo(struct Student student){
+void printStudentInfo(struct Student student) {
     printf("Имя студента: %s\n", student.name);
     printf("Оценка по математике: %d\n", student.math);
     printf("Оценка по физике: %d\n", student.phy);
     printf("Оценка по информатике: %d\n", student.inf);
     printf("Общий балл: %d\n", student.sum);
-} 
+}
 
-void generateRandomName(char *mas, int len){
+void generateRandomName(char *mas, int len) {
     char alf[] = "QWERTYUIOPLKMJNHBGVFCDXSZA";
     for (int i = 0; i < len - 1; i++) {
         mas[i] = alf[rand() % (sizeof(alf) - 1)];
@@ -37,64 +37,87 @@ void generateRandomName(char *mas, int len){
     mas[len - 1] = '\0';
 }
 
-void SelectSort(int n,struct Student *students){
+void RandomStudents(struct Student *students){
+    for (int i = 0; i < N; i++) {
+        char randomName[10];
+        generateRandomName(randomName, sizeof(randomName));
+        int math = rand() % 101;
+        int phy = rand() % 101;
+        int inf = rand() % 101;
+        students[i] = AddStudent(randomName, math, phy, inf);
+    }
+}
+
+void SelectSort(int n, struct Student *students, int (*compare)(struct Student, struct Student)) {
     for (int i = 0; i < n; i++) printStudentInfo(students[i]);
     printf("-----------------------------------------------------------------------\n");
 
     int index = 0;
     struct Student swap;
-    for(int i = 0; i < n - 1; i++){
+    for (int i = 0; i < n - 1; i++) {
         index = i;
-        for(int j = i + 1; j  < n; j++){
-            if(students[j].sum > students[index].sum)
-                index = j;}
-        if (index != i){
+        for (int j = i + 1; j < n; j++) {
+            if (compare(students[j], students[index]) < 0)
+                index = j;
+        }
+        if (index != i) {
             swap = students[index];
             students[index] = students[i];
-            students[i] = swap;   
+            students[i] = swap;
         }
     }
     for (int i = 0; i < n; i++) printStudentInfo(students[i]);
 }
 
-void CountingSort(int n,struct Student *students) {
+void swap(struct Student *a, struct Student *b) {
+    struct Student temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(struct Student *students, int low, int high, int (*compare)(struct Student, struct Student)) {
+    int pivot = students[high].sum; 
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (compare(students[j], students[high]) < 0) { 
+            i++;
+            swap(&students[i], &students[j]);
+        }
+    }
+    swap(&students[i + 1], &students[high]);
+    return i + 1;
+}
+
+void quicksort(struct Student *students, int low, int high, int (*compare)(struct Student, struct Student)) {
+    if (low < high) {
+        int pi = partition(students, low, high, compare);
+        quicksort(students, low, pi - 1, compare);
+        quicksort(students, pi + 1, high, compare);
+    }
+}
+
+void QuickSort(int n, struct Student *students, int (*compare)(struct Student, struct Student)) {
     for (int i = 0; i < n; i++) printStudentInfo(students[i]);
     printf("-----------------------------------------------------------------------\n");
-
-    int* count = (int*)calloc(MAX + 1, sizeof(int));
-
-    for (int i = 0; i < n; i++) 
-        count[students[i].sum]++;
-    int index = 0;
-    for (int i = MAX; i >= 0; i--) {
-        while (count[i] > 0) {
-            students[index++].sum = i;
-            count[i]--;
-        }
-    }
-    free(count);
+    quicksort(students, 0, n - 1, compare);
     for (int i = 0; i < n; i++) printStudentInfo(students[i]);
 }
 
+int compareBySum(struct Student a, struct Student b) {return b.sum - a.sum;}
+int compareByPhy(struct Student a, struct Student b) {return b.phy - a.phy;}
+int compareByMath(struct Student a, struct Student b) {return b.math - a.math;}
+int compareByInf(struct Student a, struct Student b) {return b.inf - a.inf;} 
 
-int main(){
+int main() {
     srand(time(NULL));
 
-    struct Student students[N]; 
-    int random_number = 2 + rand() % (63);
-    char randName[random_number];
+    struct Student students[N];
     int n = 5;
-    
-    for (int i = 0; i < N; i++) {
-        char randomName[10];
-        generateRandomName(randomName, sizeof(randomName));
-        int math = rand() % 101;       
-        int phy = rand() % 101;
-        int inf = rand() % 101;
-        students[i] = AddStudent(randomName, math, phy, inf);
-    }
+    RandomStudents(students);
 
-    
-    CountingSort(n,students);
-    
+    QuickSort(n, students, compareByMath);
+
+
+    return 0;
 }
