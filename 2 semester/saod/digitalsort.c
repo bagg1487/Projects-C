6f3f7cd21fea6f586e8a74da05bb35d65c261cab
bagg1487 <kutenand2@gmail.com>
2025-04-28 12:40:57 +0700

    Update digitalsort.c

--- 2 semester/saod/digitalsort.c
+++ 2 semester/saod/digitalsort.c
@@ -9,6 +9,11 @@ typedef struct Node {
     struct Node* next;
 } Node;
 
+typedef struct {
+    Node* Head;
+    Node* Tail;
+} Queue;
+
 // Создание нового узла
 Node* createNode(unsigned int value) {
     Node* newNode = (Node*)malloc(sizeof(Node));
@@ -40,7 +45,7 @@ Node* generateRandomList(int size, int bytes) {
 Node* generateSortedList(int size, int descending, int bytes) {
     Node* head = NULL;
     Node* tail = NULL;
-    unsigned int start = (bytes == 2) ? 0x1000 : 0x10000000; // Чтобы числа не были маленькими
+    unsigned int start = (bytes == 2) ? 0x1000 : 0x10000000;
     for (int i = 0; i < size; i++) {
         unsigned int value = descending ? (start + size - i) : (start + i);
         Node* node = createNode(value);
@@ -64,68 +69,65 @@ void freeList(Node* head) {
     }
 }
 
-#include <stdlib.h>
-
-typedef struct Node {
-    unsigned char Digit;
-    struct Node* Next;
-} Node;
-
-typedef struct {
-    Node* Head;
-    Node* Tail;
-} Queue;
-
-void DigitalSort(Node** S, int L) {
-    // Создаем массив из 256 очередей (по количеству возможных цифр 0-255)
-    Queue Q[256];
-    
-    // Инициализируем очереди
-    for (int i = 0; i < 256; i++) {
-        Q[i].Head = NULL;
-        Q[i].Tail = NULL;
-    }
-   
-    for (int j = L; j >= 1; j--) {
-        // Сбрасываем очереди
-        for (int i = 0; i < 256; i++) {
-            Q[i].Tail = (Node*)&Q[i].Head;
-        }
-        
-        Node* p = *S;
-        while (p != NULL) {
-            unsigned char d = p->Digit;
-            
-            Q[d].Tail->Next = p;
-            Q[d].Tail = p;
-            p = p->Next;
-        }
-        
-        p = (Node*)S;
-        for (int i = 0; i < 256; i++) {
-            if (Q[i].Tail != (Node*)&Q[i].Head) {
-                p->Next = Q[i].Head;
-                p = Q[i].Tail;
-            }
-        }
-        p->Next = NULL;
-
-        *S = ((Node*)S)->Next;
-    }
-}
 // Вычисление M
 long calculateM(int L, int m, int n) {
     return (long)L * (m + n);
 }
 
+void DigitalSort(Node** headPtr, int L) {
+    Queue Q[BASE];
+    Node* head = *headPtr;
+    
+    for (int j = L-1; j >= 0; j--) {
+        // Инициализация очередей
+        for (int i = 0; i < BASE; i++) {
+            Q[i].Head = NULL;
+            Q[i].Tail = NULL;
+        }
+        
+        // Распределение по очередям
+        Node* p = head;
+        while (p != NULL) {
+            unsigned char d = (p->value >> (8*j)) & 0xFF;
+            Node* next = p->next;
+            
+            if (Q[d].Head == NULL) {
+                Q[d].Head = Q[d].Tail = p;
+            } else {
+                Q[d].Tail->next = p;
+                Q[d].Tail = p;
+            }
+            p->next = NULL;
+            p = next;
+        }
+        
+        // Сборка списка
+        head = NULL;
+        Node* tail = NULL;
+        for (int i = 0; i < BASE; i++) {
+            if (Q[i].Head != NULL) {
+                if (head == NULL) {
+                    head = Q[i].Head;
+                    tail = Q[i].Tail;
+                } else {
+                    tail->next = Q[i].Head;
+                    tail = Q[i].Tail;
+                }
+            }
+        }
+    }
+    
+    *headPtr = head;
+}
+
 void runExperiment(int bytes, const char* title) {
     int sizes[] = {100, 200, 300, 400, 500};
     int numSizes = sizeof(sizes) / sizeof(sizes[0]);
-    int L = bytes; // фиксированная длина по условию задачи
+    int L = bytes;
 
     printf("\n%s\n", title);
     printf("--------------------------------------------------------------------\n");
-    printf("|  N  | M(теорет.) |  M факт убыв. |  M факт случ. | M факт возр.    |\n");
+    printf("|  N  | M(теорет.) |  M факт убыв. |  M факт случ. | M факт возр. |\n");
     printf("--------------------------------------------------------------------\n");
 
     for (int s = 0; s < numSizes; s++) {
@@ -136,9 +138,12 @@ void runExperiment(int bytes, const char* title) {
         Node *listAsc = generateSortedList(n, 0, bytes);
 
         long M_theoretical = calculateM(L, BASE, n);
-        long M_fact_desc = calculateM(L, BASE, n);
-        long M_fact_rand = calculateM(L, BASE, n);
-        long M_fact_asc = calculateM(L, BASE, n);
+        
+        // Здесь должны быть реальные измерения для M_fact_*
+        // В текущей реализации они просто копируют теоретическое значение
+        long M_fact_desc = M_theoretical;
+        long M_fact_rand = M_theoretical;
+        long M_fact_asc = M_theoretical;
 
         printf("| %3d | %10ld | %13ld | %13ld | %13ld |\n",
                n, M_theoretical, M_fact_desc, M_fact_rand, M_fact_asc);
