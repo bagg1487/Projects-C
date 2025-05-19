c4ab9da7a92c186f8349104bec1204b8757ce7ff
bagg1487 <kutenand2@gmail.com>
2025-05-19 21:41:15 +0700

    Create functions.c

---
+++ 2 semester/programming/RGR/functions.c
@@ -0,0 +1,37 @@
+#include "triangle.h"
+
+int triangularNumberRecursive(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n + triangularNumberRecursive(n - 1);}
+
+bool validateInput(int argc, char *argv[]) {
+    return argc == 3;
+}
+
+int parseInput(char *n_str) {
+    return atoi(n_str);
+}
+
+void printTriangleToFile(int n, char *file) {
+    FILE *fp = fopen(file, "w");
+    if (fp == NULL) {
+        fprintf(stderr, "Ошибка открытия файла %s для записи.\n",file);
+        exit(1);
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            fprintf(fp, "# ");
+            printf("# ");
+        }
+        fprintf(fp, "\n");
+        printf("\n");
+    }
+    fclose(fp);
+}
+
+void printUsage(char *programName) {
+    fprintf(stderr, "Формат ввода: %s <n> <Название файла для вывода>\n", programName);
+}
